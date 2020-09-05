6b9c1f5d177a1b82f901061b78f37ab4f15488f3
utgwkk <utagawakiki@gmail.com>
2020-09-05 17:21:19 +0900

    base implementation

--- cpanfile
+++ cpanfile
@@ -1,4 +1,6 @@
 requires 'perl', '5.008001';
+requires 'Test2::API';
+requires 'PPI';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
--- lib/Test2/Plugin/MakeFailedTestTODO.pm
+++ lib/Test2/Plugin/MakeFailedTestTODO.pm
@@ -3,9 +3,52 @@ use 5.008001;
 use strict;
 use warnings;
 
+use PPI;
+use Test2::API qw(
+    test2_add_callback_post_load
+    test2_stack
+);
+
 our $VERSION = "0.01";
 
+my $loaded;
+my $ppi_cache = +{};
 
+sub import {
+    my ($class) = @_;
+    return if $loaded++;
+
+    test2_add_callback_post_load(sub {
+        my $hub = test2_stack()->top;
+        $hub->listen(\&listener, inherit => 1);
+    });
+}
+
+sub listener {
+    my ($hub, $event) = @_;
+
+    return unless $event->causes_fail;
+    return if $event->can('subevents'); # ignore subtest
+
+    my $trace = $event->trace;
+    my $file = $trace->file;
+    my $line = $trace->line;
+    return unless $file;
+    return unless _fail_in_test_file($file);
+
+    _make_failed_test_todo($file, $line);
+}
+
+sub _fail_in_test_file {
+    my ($file) = @_;
+    $file =~ /\.t\z/;
+}
+
+sub _make_failed_test_todo {
+    my ($file, $line) = @_;
+    my $doc = $ppi_cache->{$file} ||= PPI::Document->new($file);
+    return unless $doc;
+}
 
 1;
 __END__
